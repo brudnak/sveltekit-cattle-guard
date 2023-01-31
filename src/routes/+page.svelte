c3de88a9f0ea174ada1e4b7fe22a4ec136254ca3
andrew brudnak <brudnak@protonmail.com>
2023-01-31 10:43:04 -0700

    Revert "💙 svelte"
    This reverts commit c4dcf1240ba364396726a762974c6103c7ac2788.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import type {PageData} from './$types';
+	import type { PageData } from './$types';
+
 	export let data: PageData;
 	import OpenLdap from './ldap/+page.svelte';
 </script>
