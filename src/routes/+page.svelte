d456044f29897b567d9f26c0e33aabaeffb44aca
andrew brudnak <brudnak@protonmail.com>
2023-02-02 15:13:05 -0700

    🚧 Fix issues with create account

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,7 @@
 				<a href="/login" class="btn btn-primary">Login</a>
 				<a href="/register" class="btn btn-secondary">Register</a>
 			</div>
-	</div>
+		</div>
 	</div>
 	{/if}
 </main>
--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,6 +1,5 @@
 import { AuthApiError } from "@supabase/supabase-js";
-import { error, fail, redirect } from "@sveltejs/kit";
-import { sortUserPlugins } from "vite";
+import { fail, redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 
 export const actions: Actions = {
@@ -11,17 +10,6 @@ export const actions: Actions = {
 			email: body.email as string,
 			password: body.password as string,
 		});
-
-		let myCheck = new RegExp(
-			"^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+.)?[a-zA-Z]+.)?(suse|suse).com$"
-		);
-		let test = myCheck.test(body.email as string);
-
-		if (test === false) {
-			throw error(400, {
-				message: "Can only register a suse email address",
-			});
-		}
 
 		if (err) {
 			if (err instanceof AuthApiError && err.status === 400) {
@@ -34,6 +22,6 @@ export const actions: Actions = {
 			});
 		}
 
-		throw redirect(303, "/email");
+		throw redirect(303, "/");
 	},
 };
