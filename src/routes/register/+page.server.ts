3c1b07141ed5f125e30c8d87268cdf8439b1cb67
andrew brudnak <brudnak@protonmail.com>
2023-02-02 15:38:13 -0700

    🚧 Maybe

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,10 +1,21 @@
 import { AuthApiError } from "@supabase/supabase-js";
-import { fail, redirect } from "@sveltejs/kit";
+import { error, fail, redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 
 export const actions: Actions = {
 	register: async ({ request, locals }) => {
 		const body = Object.fromEntries(await request.formData());
+
+		let myCheck = new RegExp(
+			"^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+.)?[a-zA-Z]+.)?(suse|suse).com$"
+		);
+		let test = myCheck.test(body.email as string);
+
+		if (test === false) {
+			throw error(400, {
+				message: "Can only register a suse email address",
+			});
+		}
 
 		const { data, error: err } = await locals.sb.auth.signUp({
 			email: body.email as string,
